a9be3382606ed67897fbe834503b94d146ae645d
ahmedmohammed63 <31933954+ahmedmohammed63@users.noreply.github.com>
2017-09-19 11:02:37 -0300

    Add files via upload

---
+++ my_fermat.py
@@ -0,0 +1,27 @@
+from math import *
+
+def check_fermat (a,b,c,n):
+ power_a= pow(a,n)
+ power_b= pow(b,n)
+ power_c= pow(c,n)
+
+ if power_a + power_b == power_c:
+     print ("Holy Smokes, Fermat was WRONG!")
+ else:
+     print("No buddy that doesn't work")
+
+
+
+check_fermat(3,4,5,2)
+check_fermat(3,4,5,3) 
+
+def fermat_checker():
+  a = int(input("enter value for 'a'"))
+  b = int(input("enter value for 'b'"))
+  c = int(input("enter value for 'c'"))
+  n = int(input("enter value for 'n'"))
+  
+  check_fermat(a,b,c,n)
+
+fermat_checker()
+
